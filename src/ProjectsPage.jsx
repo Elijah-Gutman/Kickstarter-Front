b2b4cb69de0d76c1d6dbd88343e5bb04efc0c147
mkanwal@iit <70186630+mkanwal-iit@users.noreply.github.com>
2024-12-05 13:49:28 -0600

    Add new/create actions

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { ProjectsIndex } from "./ProjectsIndex";
+import { ProjectsNew } from "./assets/ProjectsNew";
 
 export function ProjectsPage() {
   const [projects, setProjects] = useState([]);
@@ -12,11 +13,19 @@ export function ProjectsPage() {
       setProjects(response.data);
     });
   };
+  const handleCreate = (params, successCallback) => {
+    console.log("handleCreate");
+    axios.post("/projects.json", params).then((response) => {
+      setProjects([...projects, response.data]);
+      successCallback();
+    });
+  };
 
   useEffect(handleIndex, []);
 
   return (
     <main>
+      <ProjectsNew onCreate={handleCreate} />
       <ProjectsIndex projects={projects} />
     </main>
   );
